f380fefb6f4471e5e6249a7b1c685bc82d244b6a
NaveenSandaru <naveensandaru2@gmail.com>
2025-04-25 10:59:08 +0530

    Update auth-route.js

--- backend/routes/auth-route.js
+++ backend/routes/auth-route.js
@@ -6,10 +6,10 @@ import { jwTokens } from '../utils/jwt-helper.js';
 
 const router = express.Router();
 
-router.post('/loginUser', async (req, res) => {
+router.post('/loginCoach', async (req, res) => {
     try {
-        const { username, password, rememberMe } = req.body;
-        const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        const { nic, password } = req.body;
+        const user = await pool.query('SELECT * FROM coach WHERE nic = $1', [nic]);
 
         if (user.rows.length === 0) {
             return res.json({ successful: false });
@@ -26,50 +26,10 @@ router.post('/loginUser', async (req, res) => {
             httpOnly: true,
             sameSite: "None",
             secure: true,
+            maxAge: 14*24*60*60*1000
         };
 
-        if (rememberMe) {
-            cookieOptions.maxAge = 14 * 24 * 60 * 60 * 1000;
-        }
-
-        res.cookie('refreshToken', tokens.refreshToken, cookieOptions);
-        return res.json({ successful: true, accessToken: tokens.accessToken });
-
-    } catch (err) {
-        console.error("Login error:", err.message);
-        return res.status(500).json({ error: "Internal server error" });
-    }
-});
-
-router.post('/loginAdmin', async (req, res) => {
-    try {
-        const { username, password, rememberMe } = req.body;
-        console.log(rememberMe);
-        const user = await pool.query('SELECT * FROM admin WHERE username = $1', [username]);
-
-        if (user.rows.length === 0) {
-            return res.json({ successful: false });
-        }
-
-        const validPassword = await bcrypt.compare(password, user.rows[0].password);
-        if (!validPassword) {
-            return res.json({ successful: false });
-        }
-
-        let tokens = jwTokens(user.rows[0].username);
-
-        const cookieOptions = {
-            httpOnly: true,
-            sameSite: "None",
-            secure: true,
-        };
-
-        if (rememberMe) {
-            cookieOptions.maxAge = 14 * 24 * 60 * 60 * 1000;
-        }
-
-        res.cookie('refreshToken', tokens.refreshToken, cookieOptions);
-        return res.json({ successful: true, accessToken: tokens.accessToken });
+        return res.json({ successful: true, accessToken: tokens.accessToken, refreshToken: tokens.refreshToken });
 
     } catch (err) {
         console.error("Login error:", err.message);
@@ -78,34 +38,23 @@ router.post('/loginAdmin', async (req, res) => {
 });
 
 
-router.get('/refresh_token', (req, res) => {
+router.post('/refresh_token', (req, res) => {
     try {
-        const refreshToken = req.cookies.refreshToken;
+        const refreshToken = req.body.refreshToken;
         if (!refreshToken) {
             return res.json(false);
         }
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY, (error, user) => {
             if (error) return res.status(403).json({ error: error.message });
 
-            const {username } = user;
-            let accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '15m' });
+            const {nic } = user;
+            let accessToken = jwt.sign({ nic }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '15m' });
 
-            res.json({accessToken, username});
+            res.json({accessToken});
         });
     } catch (err) {
         console.error(err.message);
         return res.json(false);
-    }
-});
-
-
-/* Logout function from the frontend calls here */
-router.delete('/delete_token', (req, res) => {
-    try {
-        res.clearCookie('refreshToken');
-        return res.status(200).json({ message: 'Refresh token deleted' });
-    } catch (error) {
-        res.status(401).json({ error: error.message });
     }
 });
 
